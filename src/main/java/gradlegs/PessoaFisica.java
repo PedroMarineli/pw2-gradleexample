248e8db22b5610b5ee05bc9f4fbd7c806d9d8e44
PedroMarineli <phmo.1606@gmail.com>
2022-05-02 17:01:26 +0000

    lppiko

---
+++ src/main/java/gradlegs/PessoaFisica.java
@@ -0,0 +1,13 @@
+package gradlegs;
+
+public class Pessoa {
+    private String cpf;
+
+    public void setNome(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return cpf;
+    }
+}
