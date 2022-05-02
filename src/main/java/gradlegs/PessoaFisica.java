7e61d00ab0311299210ec3cf98a95bc5d881f809
PedroMarineli <phmo.1606@gmail.com>
2022-05-02 17:47:14 +0000

    luy

--- src/main/java/gradlegs/PessoaFisica.java
+++ src/main/java/gradlegs/PessoaFisica.java
@@ -1,13 +1,5 @@
 package gradlegs;
 
 public class PessoaFisica extends Pessoa{
-    private String cpf;
-
-    public void setNome(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return cpf;
-    }
+   
 }
