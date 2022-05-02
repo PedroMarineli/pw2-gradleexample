a71b14c728d2d32bd7d12c15dd818565b3d24a52
PedroMarineli <phmo.1606@gmail.com>
2022-05-02 17:56:58 +0000

    sdfdsf

--- src/main/java/gradlegs/PessoaFisica.java
+++ src/main/java/gradlegs/PessoaFisica.java
@@ -1,5 +1,11 @@
 package gradlegs;
 
 public class PessoaFisica extends Pessoa{
-   
+   public void setNumRf(string numRf) {
+       this.numRf = numRf;
+   }
+
+   public String getNumRf() {
+       return this.numRf;
+   }
 }
